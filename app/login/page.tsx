5f3338fe3d4957afbac667ede272ee33f840e8c9
justiceeby4 <114813288+justiceeby4@users.noreply.github.com>
2026-09-16 04:09:57 +0100

    Update page.tsx

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,8 +1,7 @@
 "use client";
 
 import { useState } from "react";
-import { createClient } from "../../utils/supabase/client";
-
+import { createClient } from "../utils/supabase/client";
 export default function LoginPage() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
