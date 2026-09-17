67476be936eb693cef23a7a7fc517452eee8e07d
justiceeby4 <114813288+justiceeby4@users.noreply.github.com>
2026-09-17 18:29:40 +0100

    Update store.ts

--- lib/store.ts
+++ lib/store.ts
@@ -118,6 +118,8 @@ export const products = [
   },
 ];
 
+export type Product = (typeof products)[number];
+
 export function money(value: number) {
   return `₦${value.toLocaleString("en-NG")}`;
 }
